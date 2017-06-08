403806eaa333f6ef43b2488b0745f83a68689159
susan <786464180@qq.com>
2017-06-08 15:14:11 +0800

    第一次推代码

---
+++ js/sui-test1.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2017/6/6.
+ */
+
+$('#page1').click(function () {
+   console.log("page1");
+});
+
+
+$('#page2').click(function () {
+    console.log("page2");
+});
---
+++ js/tour-degital.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2017/6/8.
+ */
+
+//日历
+$("#time_input").calendar({
+    value: ['2017-6-8']
+});
+
+$("#datetime-picker").datetimePicker({
+    value: ['1985', '12', '04', '9', '34']
+});
